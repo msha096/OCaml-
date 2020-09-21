418f19318e0cd8ce44e596bd7a12f84d6f9d1436
msha096 <msha096@uottawa.ca>
2020-09-21 00:10:51 -0400

    update

--- example code/t3.ml
+++ example code/t3.ml
@@ -1,7 +1,12 @@
 (* function works as argument
 Notice the type of twice is ((int -> int) * int) -> int.*)
+(* use tuple as fucntion twice's argument
 let twice ((f: int-> int),(x:int)): int = f (f x)
 let double (x:int): int = x*2
-let quad (x:int): int = twice (double,x);;
+let quad (x:int): int = twice (double, x);; works as tuple
+*)
+let twice (f: int-> int) (x:int): int = f (f x)
+let double (x:int): int = x*2
+let quad (x:int): int = twice double x;; (* two aruguments*)
 
 print_int (quad 5);;
